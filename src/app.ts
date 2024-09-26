cf64fbd7ac79d56403ef206ff73ee13eef86fa98
Atanas Atanasov <scriptex.bg@gmail.com>
2024-09-26 16:27:59 +0300

    Use CORS options

--- src/app.ts
+++ src/app.ts
@@ -38,10 +38,33 @@ app.use('/uploads', express.static('uploads'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+const whitelist = ['http://localhost:3000'];
+const corsOptions = {
+	origin(origin: string | undefined, callback: (error: Error | null, value: boolean) => unknown) {
+		if (origin && whitelist.indexOf(origin) !== -1) {
+			callback(null, true);
+		} else {
+			callback(null, false);
+		}
+	},
+	methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
+	optionsSuccessStatus: 200,
+	credentials: true,
+	allowedHeaders: [
+		'Content-Type',
+		'Authorization',
+		'X-Requested-With',
+		'device-remember-token',
+		'Access-Control-Allow-Origin',
+		'Origin',
+		'Accept'
+	]
+};
+
 /**
  * Setup CORS
  */
-app.use(cors());
+app.use(cors(corsOptions));
 
 /**
  * Setup routes
